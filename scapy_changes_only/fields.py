766a28637f710861ec86eb899f11735676a8b7e0
Ori Levi <ori1991@gmail.com>
2017-03-27 22:41:21 +0300

    make change as standalone modules

--- scapy_changes_only/fields.py
+++ scapy_changes_only/fields.py
@@ -1,18 +1,25 @@
+import struct
+from scapy.config import conf
+from scapy.fields import Field
+
+
 # YORE
 class YOIPField(Field):
     def __init__(self, name, default):
-        Field.__init__(self, name, default, "2s")
+        super(self.__class__, self).__init__(name, default, '2s')
+
     def h2i(self, pkt, x):
-            if type(x) is str:
-                a,b = x.split(".")
-                return struct.unpack(">H", struct.pack(">BB", int(a), int(b)))[0]
-            elif type(x) is list:
-                x = [self.h2i(pkt, n) for n in x] 
-            return x
+        if type(x) is basestring:
+            a, b = x.split(".")
+            return struct.unpack(">H", struct.pack(">BB", int(a), int(b)))[0]
+        elif type(x) is list:
+            x = [self.h2i(pkt, n) for n in x]
+        return x
+
     def resolve(self, x):
         if self in conf.resolve:
-            (a,b) = struct.unpack(">BB", x)
-            return "%d.%d" % (a,b)
+            (a, b) = struct.unpack(">BB", x)
+            return "%d.%d" % (a, b)
 
     def i2m(self, pkt, x):
         return struct.pack(">H", x)
@@ -21,23 +28,24 @@ class YOIPField(Field):
         return struct.unpack(">H", x)[0]
 
     def any2i(self, pkt, x):
-        return self.h2i(pkt,x)
+        return self.h2i(pkt, x)
 
     def i2repr(self, pkt, x):
         return self.resolve(self.i2h(pkt, x))
 
     def i2h(self, pkt, x):
-        a,b = struct.pack(">H", x)
-        return "%d.%d" %(ord(a), ord(b))
+        a, b = struct.pack(">H", x)
+        return "%d.%d" % (ord(a), ord(b))
 
     def i2repr(self, pkt, x):
-        a,b = struct.pack(">H", x)
-        return "%d.%d" %(ord(a), ord(b))
+        a, b = struct.pack(">H", x)
+        return "%d.%d" % (ord(a), ord(b))
 
     def m2h(self, pkt, x):
-        a,b = x[0], x[1]
+        a, b = x[0], x[1]
         return "%d.%d" % (ord(a), ord(b))
 
     def randval(self):
         return 4000
+
 # YORE
--- scapy_changes_only/l2.py
+++ scapy_changes_only/l2.py
@@ -1,21 +1,36 @@
+import struct
+
+from fields import YOIPField
+from scapy.config import conf
+from scapy.packet import Packet
+from scapy.sendrecv import srp1
+from scapy.layers.inet import ARP
+from scapy.arch import get_if_hwaddr
+from scapy.data import ETHER_BROADCAST, ETHER_TYPES, ETHER_ANY
+from scapy.fields import XShortField, ByteField, ShortEnumField
+from scapy.layers.l2 import (SourceMACField, DestMACField, XShortEnumField,
+                             Dot3, ARPSourceMACField, MACField)
+
 # YORE
-conf.netcache.new_cache("yoarp_cache", 120) # cache entries expire after 120s
+conf.netcache.new_cache("yoarp_cache", 120)  # cache entries expire after 120s
+
 
 # YORE
 @conf.commands.register
 def register_yoip(yoip):
     conf.yoip = yoip
- 
+
+
 register_yoip("0.0")
+
 
 # YORE
 @conf.commands.register
-def getmacbyyoip(yoip, chainCC=0):
- 
+def get_mac_by_yoip(yoip, chain_cc=0):
     """Return MAC address corresponding to a given YOIP address"""
 
     # YORE TODO: is 0.0.0.0 a good enough default? I think so.
-    iff,a,gw = conf.route.route("0.0.0.0")
+    iff, a, gw = conf.route.route("0.0.0.0")
 
     src_mac = get_if_hwaddr(iff)
 
@@ -24,14 +39,13 @@ def getmacbyyoip(yoip, chainCC=0):
     except:
         myyoip = None
 
-    if (myyoip == yoip):
+    if myyoip == yoip:
         return src_mac
 
-
-    if (yoip == "0.0"):
+    if yoip == "0.0":
         return "00:00:00:00:00:00"
 
-    if (yoip == "255.255"):
+    if yoip == "255.255":
         return "ff:ff:ff:ff:ff:ff"
 
     mac = conf.netcache.yoarp_cache.get(yoip)
@@ -40,38 +54,46 @@ def getmacbyyoip(yoip, chainCC=0):
 
     print "GETTING MAC BY IP"
 
-    pkt = Ether(src=src_mac, dst=ETHER_BROADCAST)/YOARP(op="who-has", hwsrc=src_mac, psrc=conf.yoip, pdst=yoip)
+    pkt = (
+        Ether(src=src_mac, dst=ETHER_BROADCAST) /
+        YOARP(op="who-has", hwsrc=src_mac, psrc=conf.yoip, pdst=yoip)
+    )
     # TODO: Do we need to fix anything in "answers"?
 
     res = srp1(pkt,
-                iface = iff,
-                timeout=5,
-                verbose=0,
-                retry=3,
-                chainCC=chainCC,
-                nofilter=1 ) 
-                
+               iface=iff,
+               timeout=5,
+               verbose=0,
+               retry=3,
+               chainCC=chain_cc,
+               nofilter=1)
+
     if res is not None:
         mac = res.payload.hwsrc
         conf.netcache.yoarp_cache[yoip] = mac
         return mac
 
 
-
 class Ether(Packet):
     name = "Ethernet"
-    fields_desc = [ DestMACField("dst"),
-                    SourceMACField("src"),
-                    XShortEnumField("type", 0x9000, ETHER_TYPES) ]
+    fields_desc = [
+        DestMACField("dst"),
+        SourceMACField("src"),
+        XShortEnumField("type", 0x9000, ETHER_TYPES)
+    ]
+
     def hashret(self):
-        return struct.pack("H",self.type)+self.payload.hashret()
+        return struct.pack("H", self.type) + self.payload.hashret()
+
     def answers(self, other):
-        if isinstance(other,Ether):
+        if isinstance(other, Ether):
             if self.type == other.type:
                 return self.payload.answers(other.payload)
         return 0
+
     def mysummary(self):
         return self.sprintf("%src% > %dst% (%type%)")
+
     @classmethod
     def dispatch_hook(cls, _pkt=None, *args, **kargs):
         if _pkt and len(_pkt) >= 14:
@@ -92,26 +114,32 @@ class Ether(Packet):
 # YORE
 class YOARP(Packet):
     name = "YOARP"
-    fields_desc = [ XShortField("hwtype", 0x0001),
-                    XShortEnumField("ptype",  0x9999, ETHER_TYPES),
-                    ByteField("hwlen", 6),
-                    ByteField("plen", 2),
-                    ShortEnumField("op", 1, {"who-has":1, "is-at":2, "RARP-req":3, "RARP-rep":4, "Dyn-RARP-req":5, "Dyn-RAR-rep":6, "Dyn-RARP-err":7, "InARP-req":8, "InARP-rep":9}),
-                    ARPSourceMACField("hwsrc"),
-                    YOIPField("psrc", "0.0"),
-                    MACField("hwdst", ETHER_ANY),
-                    YOIPField("pdst", "0.0") ]
+    fields_desc = [
+        XShortField("hwtype", 0x0001),
+        XShortEnumField("ptype", 0x9999, ETHER_TYPES),
+        ByteField("hwlen", 6),
+        ByteField("plen", 2),
+        ShortEnumField("op", 1,
+                       {"who-has": 1, "is-at": 2, "RARP-req": 3,
+                        "RARP-rep": 4, "Dyn-RARP-req": 5,
+                        "Dyn-RAR-rep": 6, "Dyn-RARP-err": 7,
+                        "InARP-req": 8, "InARP-rep": 9}),
+        ARPSourceMACField("hwsrc"),
+        YOIPField("psrc", "0.0"),
+        MACField("hwdst", ETHER_ANY),
+        YOIPField("pdst", "0.0")
+    ]
     who_has = 1
     is_at = 2
 
     def extract_padding(self, s):
-        return "",s
+        return "", s
 
     def answers(self, other):
-        if isinstance(other,YOARP):
-            if ( (self.op == self.is_at) and
-                 (other.op == self.who_has) and
-                 (self.psrc == other.pdst) ):
+        if isinstance(other, YOARP):
+            if ((self.op == self.is_at) and
+                    (other.op == self.who_has) and
+                    (self.psrc == other.pdst)):
                 return 1
         return 0
 
@@ -121,5 +149,6 @@ class YOARP(Packet):
             return ARP
         return cls
 
+
 # YORE
-conf.neighbor.register_l3(Ether, YOARP, lambda l2,l3: getmacbyyoip(l3.pdst))
+conf.neighbor.register_l3(Ether, YOARP, lambda l2, l3: get_mac_by_yoip(l3.pdst))
