95cb332faba9172e29f5311a82d2d32a00b680bf
Ori Levi <ori1991@gmail.com>
2017-05-13 19:24:14 +0300

    move all files under yore directory __init.py__ : contains only the relevant modules

---
+++ scapy_changes_only/yore/__init__.py
@@ -0,0 +1,13 @@
+from inet import *
+from l2 import *
+from yore_fields import *
+
+__all__ = [
+    'YO',
+    'RE',
+    'YOIPField',
+    'register_yoip',
+    'get_mac_by_yoip',
+    'Ether',
+    'YOARP',
+]
